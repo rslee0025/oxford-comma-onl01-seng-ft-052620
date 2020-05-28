d738c408350b10e429bb4cf35be34fb4eff3a80b
Rachel S. Lee <rslee0025@gmail.com>
2020-05-28 22:14:40 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,29 @@
+=begin
 def oxford_comma(array)
+  
+  case array.length
+    when 1 
+      "#{array[0]}"
+    when 2 
+      array[0..1].join(" and ")
+    else
+      array[0...-1].join(", ") << ", and #{array[-1]}"
+    end
+end  
+=end
 
-end
+
+
+def oxford_comma(array)
+  
+  case array.length
+    when 1 
+      "#{array[0]}"
+    when 2 
+      array[0..1].join(" and ")
+    else
+      array[0...-1].join(", ") << ", and #{array[-1]}" #0...-1 the third period is telling the array to add a comma up to the second to last element. After that the << is going to add "", and" before the last element
+    end
+end  
+
+
